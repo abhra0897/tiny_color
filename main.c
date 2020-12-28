c48c26744414a4c6a07cb07837e8ce8c75847869
Avra Mitra <abhra0897@gmail.com>
2020-12-28 17:31:33 +0530

    added license info

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
+/*
+MIT License
+Copyright (c) 2020 Avra Mitra
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
@@ -11,8 +31,6 @@
 #define    LED_GREEN   PB1
 #define    LED_BLUE    PB0
 #define    LED_RED     PB2
-/* Software PWM Total time = on time + off time (in us) */
-#define TOTAL_TIME 1024
 
 static volatile uint16_t g_tick_counter = 0;                //Counts ticks
 
